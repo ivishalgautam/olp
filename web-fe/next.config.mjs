03c47620ad9627653c6a00c8a0e5866aa5ae958f
Vishal Gautam <ivishalgautamm@gmail.com>
2024-05-04 10:51:28 +0530

    change remote urls

--- web-fe/next.config.mjs
+++ web-fe/next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "web.onlineparts.shop",
+        hostname: "api.onlineparts.shop",
         port: "",
       },
     ],
