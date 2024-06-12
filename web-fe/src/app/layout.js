4289ce7cc15469a68673ec753c953ecd4a8e3a33
Vishal Gautam <ivishalgautamm@gmail.com>
2024-06-12 12:10:40 +0530

    added seo related content

--- web-fe/src/app/layout.js
+++ web-fe/src/app/layout.js
@@ -4,6 +4,7 @@ import QueryProvider from "@/components/QueryClientProvider";
 import { Toaster } from "sonner";
 import Context from "@/store/context";
 import Layout from "@/components/layout";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata = {
   title: "Online Parts Portal | JCB Spares Parts - Onlineparts.Shop",
@@ -15,11 +16,17 @@ export const metadata = {
       "Get all the JCB Spares Parts at one place and get them fast. Our list of JCB Genuine Parts, Earthmoving Spare Parts",
     images: ["https://onlineparts.shop/og_img.jpeg"],
   },
+  verification: {
+    google: "8h3AdhyQjMVo6rNeDBE7Bni4OaYT9sAi8iJHh5z1-4U",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <GoogleTagManager gtmId="G-PB2CFZ5R74" />
+      </head>
       <body
         className={`${GeistSans.className} overflow-x-hidden`}
         suppressHydrationWarning={true}
