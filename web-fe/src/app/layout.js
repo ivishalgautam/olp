2f30c3f040460297cf1a7a9dd4cbd152acc5e298
Vishal Gautam <ivishalgautamm@gmail.com>
2024-06-14 15:42:15 +0530

    updated metadata in home, about, contact

--- web-fe/src/app/layout.js
+++ web-fe/src/app/layout.js
@@ -7,14 +7,23 @@ import Layout from "@/components/layout";
 import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata = {
-  title: "Online Parts Portal | JCB Spares Parts - Onlineparts.Shop",
+  title:
+    "Online Parts Shop - JCB Spare Parts in India | Earthmoving Spare Parts",
   description:
-    "Get all the JCB Spares Parts at one place and get them fast. Our list of JCB Genuine Parts, Earthmoving Spare Parts",
+    "Online Parts Shop - Leading manufacture of authentic JCB Spare parts and Earthmoving spare parts in India. Experience top-quality products and exceptional customer support.",
+  keywords:
+    "jcb spare parts in india, jcb spare parts, Earthmoving spare parts in india jcb spare parts of india, Earthmoving spare parts  jcb spare parts in faridabad, jcb parts, jcb genuine parts, jcb spare parts online, spare parts of jcb, jcb engine spare parts, jcb spare parts manufacturer, jcb spare parts suppliers, jcb spare parts manufacturer in india,jcb spare parts manufacturer in faridabad, jcb spare parts price, Earthmoving spare parts wholesale suppliers, jcb spares suppliers, jcb spares wholesale suppliers, Earthmoving spare manufacturers in india",
+  authors: [{ name: "Online Parts Shop" }],
+  publishers: "Online Parts Shop",
   openGraph: {
-    title: "Online Parts Portal",
+    title:
+      "Online Parts Shop - JCB Spare Parts in India | Earthmoving Spare Parts",
     description:
-      "Get all the JCB Spares Parts at one place and get them fast. Our list of JCB Genuine Parts, Earthmoving Spare Parts",
+      "Online Parts Shop - Leading manufacture of authentic JCB Spare parts and Earthmoving spare parts in India. Experience top-quality products and exceptional customer support.",
     images: ["https://onlineparts.shop/og_img.jpeg"],
+  },
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_SITE_URL,
   },
   verification: {
     google: "8h3AdhyQjMVo6rNeDBE7Bni4OaYT9sAi8iJHh5z1-4U",
