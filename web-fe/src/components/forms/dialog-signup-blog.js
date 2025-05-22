f955ee4b85e17a69906cf62bde1ff7dbd936ed34
Vishal Gautam <ivishalgautamm@gmail.com>
2025-05-22 17:35:37 +0530

    added registrations forms

--- web-fe/src/components/forms/dialog-signup-blog.js
+++ web-fe/src/components/forms/dialog-signup-blog.js
@@ -155,7 +155,7 @@ export default function DialogSignUpBlogForm({ setIsModal }) {
             <button
               type="submit"
               disabled={createMutation.isLoading}
-              className="w-full rounded-md bg-[#12548D] px-4 py-3 font-semibold text-white transition-colors hover:bg-blue-800"
+              className="w-full rounded-md bg-[#12548D] px-4 py-2 font-semibold text-white transition-colors hover:bg-blue-800"
             >
               {createMutation.isLoading
                 ? "Signing Up..."
